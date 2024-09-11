024a519e248bfda74450df81f75c06e1cfaa0baa
matheusmontelro <ferreiramatheus10@gmail.com>
2024-09-11 16:09:41 -0300

    Update startup.js

--- startup.js
+++ startup.js
@@ -1,90 +1,78 @@
-const request = require('request-promise');
 const axios = require('axios');
 const config = require('./config');
 const logger = require('./util/logger');
 
 const { exec } = require('child_process');
-
 const chalk = require('chalk');
 
 const DeviceModel = require('./Models/device');
 const Device = DeviceModel(config.sequelize);
 
 async function startAllSessions() {
+    const host = config?.host ? `${config?.host}:${config?.port}` : config?.host_ssl;
+    const sessions = await Device.findAll();
 
-	const host = config?.host ? `${config?.host}:${config?.port}` : config?.host_ssl;
-	const sessions = await Device.findAll();
-
-	if (sessions != null) {
-		
-        // update all devices without where
+    if (sessions != null) {
+        // Atualizar todos os dispositivos sem where
         await Device.update({
-			
-			qrCode: '',
-			attempts: 0,
-			urlCode: '',
-			attempts_start: 0,
-			last_start: 'NULL',
+            qrCode: '',
+            attempts: 0,
+            urlCode: '',
+            attempts_start: 0,
+            last_start: 'NULL',
+            state: 'DISCONNECTED',
+            status: 'notLogged',
+            updated_at: new Date()
+        }, { where: {} });
 
-			state: 'DISCONNECTED',
-			status: 'notLogged',
-			
-			updated_at: new Date()
-		}, { where: {} });
+        logger.info(`Iniciando ${sessions.length} sessões...`);
 
-		logger.info(`Iniciando ${sessions.length} sessões...`);
+        async function startSession(object) {
+            const sessionData = {
+                ...object.dataValues,
+                apitoken: config.token,
+            };
 
-		async function startSession(object) {
+            try {
+                await axios.post(`${host}/start`, {
+                    session: sessionData.session,
+                    wh_connect: sessionData.wh_connect,
+                    wh_qrcode: sessionData.wh_qrcode,
+                    wh_status: sessionData.wh_status,
+                    wh_message: sessionData.wh_message,
+                    AutoRejectCall: sessionData.AutoRejectCall,
+                    AnswerMissedCall: sessionData.AnswerMissedCall
+                }, {
+                    headers: {
+                        apitoken: sessionData.apitoken,
+                        sessionkey: sessionData.sessionkey
+                    }
+                });
 
-			const sessionData = {
-				...object.dataValues,
-				apitoken: config.token,
-			}
+                console.log(chalk.green(`[SESSION] ${chalk.bold.green(sessionData.session)} - ${chalk.bold.green(`Iniciada com sucesso!`)}`));
 
-			try {
+            } catch (error) {
+                console.log(chalk.red(`[ERROR] Falha ao iniciar a sessão ${sessionData.session}: ${error.message}`));
+            }
+        }
 
-				await axios.post(`${host}/start`, {
-					session: sessionData.session,
-					wh_connect: sessionData.wh_connect,
-					wh_qrcode: sessionData.wh_qrcode,
-					wh_status: sessionData.wh_status,
-					wh_message: sessionData.wh_message,
-					AutoRejectCall: sessionData.AutoRejectCall,
-					AnswerMissedCall: sessionData.AnswerMissedCall
-				}, {
-					headers: {
-						apitoken: sessionData.apitoken,
-						sessionkey: sessionData.sessionkey
-					}
-				});
-		
-				console.log(chalk.green(`[SESSION] ${chalk.bold.green(sessionData.session)} - ${chalk.bold.green(`Iniciada com sucesso!`)}`));
+        // Iniciar sessões sequencialmente com um intervalo de 15 segundos
+        for (let i = 0; i < sessions.length; i++) {
+            sessionData = sessions[i];
+            await startSession(sessionData);
 
-			} catch (error) {
-				console.log(error);
-			}
-		}
-
-		for (let i = 0; i < sessions.length; i++) {
-
-			sessionData = sessions[i];
-			await startSession(sessionData);
-
-			if (i < sessions.length - 1) {
-				await new Promise(resolve => setTimeout(resolve, 15000)); // 15 seconds
-			}
-
-		}
-
-	}else{
-		//clear instances folder
-		exec('rm -rf ./instances/*', (err) => {
-			if (err) {
-				console.error("Erro ao apagar instances folder:", err);
-			}
-		});
-
-	}
+            if (i < sessions.length - 1) {
+                await new Promise(resolve => setTimeout(resolve, 15000)); // 15 segundos
+            }
+        }
+    } else {
+        // Limpar pasta instances
+        exec('rm -rf ./instances/*', (err) => {
+            if (err) {
+                console.error("Erro ao apagar a pasta instances:", err);
+            }
+        });
+    }
 }
 
 module.exports.startAllSessions = startAllSessions;
